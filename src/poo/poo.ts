fca34ef5443e0b97d553eac7e6247f3a97589de1
Gadiel Monteabaro <silvagadiel96@gmail.com>
2026-02-16 20:32:18 -0300

    feat:change exercise and add abstract class

--- src/poo/poo.ts
+++ src/poo/poo.ts
@@ -33,3 +33,39 @@ export class Restar extends Operacion {
     this.resultado = this.valorA - this.valorB;
   }
 }
+
+export abstract class Forma {
+  protected nombre: string;
+
+  constructor(nombre: string) {
+    this.nombre = nombre;
+  }
+
+  setNombre(value: string) {
+    this.nombre = value;
+  }
+
+  getNombre(): string {
+    return this.nombre;
+  }
+
+  abstract perimetro(): number;
+  abstract area(): number;
+}
+
+export class Circulo extends Forma {
+  private radio: number;
+
+  constructor(radio: number) {
+    super("Circulo");
+    this.radio = radio;
+  }
+
+  area(): number {
+    return Math.PI * this.radio ** 2;
+  }
+
+  perimetro(): number {
+    return 2 * Math.PI * this.radio;
+  }
+}
